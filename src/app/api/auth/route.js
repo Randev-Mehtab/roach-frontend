8be6d83b97ab6cde2f6e3a11ca515f13d00a3130
Mehtab Randev <randevmehtab127uk@gmail.com>
2025-03-24 04:36:03 +0800

    Correct NextAuth route configuration

--- src/app/api/auth/route.js
+++ src/app/api/auth/route.js
@@ -1,1 +1,13 @@
-export { GET, POST } from "next-auth";
+import NextAuth from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+
+const handler = NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
